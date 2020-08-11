7c3a21ff9e188912b61909db32f9f96c2e3c4d00
Nguyen Huynh <huynhn.tma@orangetheoryfitness.com>
2020-08-11 15:49:52 +0700

    Updates

---
+++ precache-manifest.867bae756d39d1c942d5a4907ba088df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ace29eb9b03068a892ce92633601ec",
+    "url": "/web-bluetooth/index.html"
+  },
+  {
+    "revision": "876167af7e149f06a3a2",
+    "url": "/web-bluetooth/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c11430692810beab56c",
+    "url": "/web-bluetooth/static/js/2.2bfafe6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-bluetooth/static/js/2.2bfafe6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876167af7e149f06a3a2",
+    "url": "/web-bluetooth/static/js/main.7b93a400.chunk.js"
+  },
+  {
+    "revision": "e5e5dfaf24ee534e220a",
+    "url": "/web-bluetooth/static/js/runtime-main.926cafc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-bluetooth/static/media/logo.5d5d9eef.svg"
+  }
+]);
