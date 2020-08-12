ed80094f6f078f1949710825f784c3f7665d2c04
Nguyen Huynh <huynhn.tma@orangetheoryfitness.com>
2020-08-12 15:23:13 +0700

    Add DiscoverButton to request connect by BLE

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import preval from 'preval.macro';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import DiscoveryButton from './components/bluetooth/discoveryButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <label>Build date: {preval`module.exports = new Date().toLocaleString()`}</label>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <DiscoveryButton></DiscoveryButton>
         </a>
       </header>
     </div>
---
+++ src/components/bluetooth/discoveryButton.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import _ from "lodash";
+
+export default class DiscoveryButton extends React.Component<IDiscoveryButtonProps> {
+  constructor(props: IDiscoveryButtonProps) {
+    super(props);
+    this.state = {};
+    this.onClick = this.onClick.bind(this);
+  }
+
+  async onClick(event: React.MouseEvent) {
+    try {
+      event.preventDefault();
+      console.log(`Button onClicked`);
+
+      if (_.isNil(navigator.bluetooth))
+        throw new Error("Browser doesn't support intergrating bluetooth");
+
+      let device = await navigator.bluetooth.requestDevice({
+        filters: [
+          { services: ['battery_service', 'heart_rate'] },
+          { namePrefix: 'OT' }
+        ],
+        optionalServices: ['battery_service', 'heart_rate']
+      });
+
+      if(_.isNil(device)) throw new Error('Failed to get device');
+      console.log(`Devices: `,device.id, device.name, device.uuids);
+
+      // Attempts to connect to remote GATT Server.
+      let server = await device.gatt?.connect();
+      if(_.isNil(server)) throw new Error(`Could not connect to GATT Server`);
+
+      let services = server.getPrimaryServices();
+      console.log(`Services: `,services);
+
+      (window as any).bleServices = services;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <button className="btn btn-primary" onClick={this.onClick}>
+        Discover
+      </button>
+    );
+  }
+}
+
+interface IDiscoveryButtonProps { }
